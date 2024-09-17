6120cc1656cc1ec834912b7750238c2a563c5f19
Fábio Barrufi Pagano <61845465+fabio-barrufi@users.noreply.github.com>
2024-09-16 23:16:41 -0300

    Setup inicial do projeto

---
+++ src/data-source.ts
@@ -0,0 +1,17 @@
+import "reflect-metadata"
+import { DataSource } from "typeorm"
+import { Usuario } from "./entity/Usuario"
+
+export const AppDataSource = new DataSource({
+    type: "postgres",
+    host: "localhost",
+    port: 5432,
+    username: "postgres",
+    password: "postgres",
+    database: "nodejs",
+    synchronize: true,
+    logging: false,
+    entities: [Usuario],
+    migrations: [],
+    subscribers: [],
+})
